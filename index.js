f9fa72de185c80c44b5a883f50e0a4dbfa1f37a8
bob leponge <dallas.lones@mailprotector.com>
2022-07-05 16:32:13 -0400

    spring cleaning

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-((utils, the_axios ) => {
+((utils, the_real_axios ) => {
     exports.register = function () {
         const plugin = this;
         plugin.inherits('auth/auth_base');
-        plugin.load_tls_ini();
-        plugin.load_config();
+        plugin.load_tls_ini(plugin);
+        plugin.load_config(plugin);
     }
 
-    exports.load_config = function() {
-        const plugin = this;
-
-        plugin.cfg = plugin.cfg.get('http-auth.json', () => {
-            plugin.load_config();
-        });
+    exports.load_config = function(plugin) {
+        plugin.cfg = plugin.cfg.get('http-auth.json', plugin.load_config);
     }
 
-    exports.load_tls_ini = function () {
-        const plugin = this;
-
-        plugin.tls_cfg = plugin.config.get('tls.ini', () => {
-            plugin.load_tls_ini();
-        });
+    exports.load_tls_ini = function (plugin) {
+        plugin.tls_cfg = plugin.config.get('tls.ini', plugin.load_tls_ini);
     }
 
     exports.hook_capabilities = (next, connection) => {
@@ -43,7 +35,7 @@
         if ((domain = /@([^@]+)$/.exec(user))) {
             domain = domain[1].toLowerCase();
         } else {
-            connection.logwarning(plugin, `AUTH user="${user}" error="not in required format"`);
+            plugin.logwarning(`AUTH user="${user}" error="not in required format"`);
             return next(false);
         }
     };
@@ -69,14 +61,14 @@
             checkUserValid(plugin, connection, next);
 
             const body = { user: user.toLowerCase(), pass: passwd };
-            checkAuthFromServer(axios, plugin.cfg.URL, body, buildOptions(this), connection, next);
+            checkAuthFromServer(axios, plugin.cfg.URL, body, buildOptions(plugin), connection, next);
         }
     };
 
-    // haraka's required exports func
-    exports.check_plain_passwd = buildCheckPlainPasswdFunc(the_axios);
-    
     exports.buildOptions = buildOptions;
     exports.checkUserValid = checkUserValid;
+    exports.checkAuthFromServer = checkAuthFromServer;
     exports.buildCheckPlainPasswdFunc = buildCheckPlainPasswdFunc;
+    // haraka's required exports func
+    exports.check_plain_passwd = buildCheckPlainPasswdFunc(the_real_axios);
 })(require('haraka-utils'), require('axios'));
