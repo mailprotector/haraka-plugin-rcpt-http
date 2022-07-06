8c879f5d081010be46b304a19037b714cc05d1cb
bob leponge <dallas.lones@mailprotector.com>
2022-07-06 14:14:52 -0400

    Revert "donezo"
    This reverts commit 0ae343e5f6f7c2f921a25988622e5e1e127eb94b.

--- index.js
+++ index.js
@@ -1,55 +1,82 @@
-((utils, the_axios ) => {
-    exports.register = function () {
-        const plugin = this;
-        plugin.inherits('auth/auth_base');
-        plugin.load_tls_ini();
+(() => {
+
+  exports.register = function () {
+    this.register_hook('rcpt', 'rcpt_http');
+    this.load_config();
+  }
+
+  exports.load_config = function () {
+    const plugin = this;
+
+    try {
+      plugin.cfg = this.config.get('rcpt-http.json', plugin.load_config);
+    } catch (err) { }
+
+    if (plugin.cfg.URL == undefined) {
+      plugin.cfg.URL = process.env.RCPT_CHECK_URL;
     }
 
-    exports.load_tls_ini = function () {
-        const plugin = this;
-        plugin.tls_cfg = plugin.config.get('tls.ini', () => {
-            plugin.load_tls_ini();
-        });
+    if (plugin.cfg.USERNAME == undefined) {
+      plugin.cfg.USERNAME = process.env.USERNAME;
     }
 
-    exports.hook_capabilities = (next, connection) => {
-        if (connection.tls.enabled) {
-            const methods = ['PLAIN', 'LOGIN'];
-            connection.capabilities.push(`AUTH ${methods.join(' ')}`);
-            connection.notes.allowed_auth_methods = methods;
+    if (plugin.cfg.PASSWORD == undefined) {
+      plugin.cfg.PASSWORD = process.env.PASSWORD;
+    }
+
+    if (plugin.cfg.USERNAME !== undefined && plugin.cfg.PASSWORD !== undefined) {
+      plugin.auth = true;
+
+      const authString = `${plugin.cfg.PASSWORD}:${plugin.cfg.PASSWORD}`;
+      const authBase64 = new Buffer.from(authString).toString('base64');
+
+      plugin.authHeaders = {
+        Authorization: `Basic ${authBase64}`,
+      };
+    } else {
+      plugin.auth = false;
+    }
+  };
+
+  function buildRcptHttp(axios, statusCodes) {
+    return function (next, connection, params) {
+      const plugin = this;
+
+      const body = {
+        email: params[0].original,
+        ip: connection.remote.ip
+      };
+
+      const options = {};
+
+      if (plugin.auth == true) {
+        options.headers = plugin.authHeaders;
+      }
+
+      axios.post(this.cfg.URL || process.env.RCPT_CHECK_URL, body, options).then(response => {
+        if (response.status >= 200 && response.status < 600) {
+          if (response.data.code == undefined) {
+            next(DENYSOFT);
+          } else {
+            next(statusCodes[response.data.code], response.data.message);
+          }
+        } else {
+          next(DENYSOFT, `Backend failure. Please, retry later ${response.status}`);
         }
-        next();
-    }
-
-    const buildCheckPlainPasswdFunc = (axios) => {
-        return function (connection, user, passwd, next) {
-            let domain;
-            if ((domain = /@([^@]+)$/.exec(user))) {
-                domain = domain[1].toLowerCase();
-            } else {
-                // AUTH user not in user@domain.com format
-                connection.logerror(this, `AUTH user="${user}" error="not in required format"`);
-                return next(false);
-            }
-
-            const options = {};
-            if (plugin.auth == true) {
-                options.headers = plugin.authHeaders;
-            }
-
-            const body = { user, pass: passwd };
-            axios.post(this.cfg.URL || process.env.AUTH_CHECK_URL, body, options).then(response => {
-                if (response.status != 200) {
-                    return next(false);
-                }
-                next(true);
-            }).catch(err => {
-                next(false);
-            })
-
-            next();
+      }).catch(err => {
+        if (err != undefined) {
+          plugin.logerror(err.message || err);
         }
-    };
+        next(DENYSOFT, 'Backend failure. Please, retry later');
+      });
+    }
+  }
 
-    exports.check_plain_passwd = buildCheckPlainPasswdFunc(the_axios);
-})(require('haraka-utils'), require('axios'));
+  exports.rcpt_http_test = buildRcptHttp;
+  exports.rcpt_http = buildRcptHttp(require('axios'), {
+    OK: OK,
+    DENYSOFT: DENYSOFT,
+    DENY: DENY
+  });
+
+})();
